d83906c878814c81ebab6fd5d0bba4533a693206
toto505 <mail@ot-online.com>
2022-11-02 09:51:45 +0900

    test

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ io.on('connection', (socket) => {
   console.log('a user connected');
 });
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+server.listen(80, () => {
+  console.log('listening on *:80');
 });
 
 io.on('connection', (socket) => {
